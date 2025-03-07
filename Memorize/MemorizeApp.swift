366decd463cd2087a86ee726873c74964de3b816
Henrique Gava <henriquegavabarreto@gmail.com>
2025-03-06 16:12:43 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Henrique Barreto on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
