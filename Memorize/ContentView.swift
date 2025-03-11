75616eaf367f0372e3a3343bce11ee5d80892d4a
Henrique Gava <henriquegavabarreto@gmail.com>
2025-03-10 18:51:52 -0700

    Refactor Add LazyVGrid and ScrollView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,19 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["🦁", "🐭", "🐯", "🐮"]
+    let emojis = ["🦁", "🐭", "🐯", "🐮", "🐸", "🦊", "🐻", "🐰"]
     let cardBaseColor: Color = .orange
     
+    // number of cards showed on the screen
+    @State var cardCount = 4
+    
     var body: some View {
-        HStack {
-            ForEach(emojis, id: \.self) { emoji in
-                CardView(faceValue: emoji)
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+    }
+    
+    // cards on the screen
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                if(index < emojis.count) {
+                    CardView(faceValue: emojis[index])
+                        .aspectRatio(2/3, contentMode: .fit)
+                }
             }
         }
         .foregroundColor(cardBaseColor)
         .padding()
     }
+    
+    // Buttons at the bottom that adjust cardCount variable
+    var cardCountAdjusters: some View {
+        HStack {
+            removeCardButton
+            Spacer()
+            addCardButton
+        }
+        .labelStyle(.iconOnly)
+        .imageScale(.large)
+        .font(.title)
+        .padding()
+    }
+    
+    func cardCountAdjuster (by offset: Int, title: String, symbol: String)  -> some View {
+        Button(title, systemImage: symbol) {
+            cardCount += offset
+        }
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    // Button - removes 1 from cardCount
+    var removeCardButton: some View {
+        cardCountAdjuster(by: -1, title: "Remove Card", symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    // Button - Adds 1 to cardCount
+    var addCardButton: some View {
+        cardCountAdjuster(by: 1, title: "Add Card", symbol: "rectangle.stack.badge.plus.fill")
+    }
 }
+
 
 struct CardView: View {
     let faceValue: String
@@ -30,17 +78,18 @@ struct CardView: View {
             // Define card base shape
             let baseShape = RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
             // Show card face up
-            if isFaceUp {
+            Group {
                 baseShape
                     .foregroundColor(.white)
                 baseShape
                     .strokeBorder(lineWidth: 3)
                 Text(faceValue)
                     .font(.largeTitle)
-            } else {
-                // Show back of the card only
-                baseShape
             }
+            .opacity(isFaceUp ? 1 : 0)
+            // Show back of the card only
+            baseShape
+                .opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {
             isFaceUp.toggle()
